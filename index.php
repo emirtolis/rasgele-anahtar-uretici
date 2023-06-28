e6988f090d502af6d9f9348c63e4bfb01926e03b
Emir Tolis <121692132+emirtolis@users.noreply.github.com>
2023-06-28 15:04:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,30 +17,31 @@ $config = file_get_contents($path_config);
 $config = str_replace('$config[\'app_key\'] = \'\';', '$config[\'app_key\'] = \''.$key.'\';', $config);
 file_put_contents($path_config, $config);
 ?>
+
+<!--  html,css,js ve php kullanıldı  -github.com/emirtolis-->
 <!DOCTYPE html>
 <html lang="tr">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href="https://github.githubassets.com/favicons/favicon.svg" class="github-favicon-svg">
-	<link rel="manifest" href="https://github.com/manifest.json">
+	<link rel="icon" href="http://github.githubassets.com/favicons/favicon.svg">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/footer.css">
 	<title>Rasgele Anahtar Oluşturucu</title>
 </head>
-<body>
-	<center>
-	<h2>Yeni Anahtar: </h2><p class="key" id="new_key"><?php echo "{$key}\r\n";?></p>
+<body style="text-align:center;"><br>
+	<h2>Rasgele Anahtar Oluşturucu </h2>
+	<input type="text" id="metin" value="<?php echo "{$key}\r\n";?>" size="33"><br><br>
 	<button onclick="window.location.reload()">Yeni Anahtar</button>
-	<br><hr>
-	<footer>
-		<h6 class="footer-text">
-			2023 By Emir Tolis <br>
-		</h6>
-	</footer><hr>
-		<a href="https://github.com/emirtolis/rasgele-anahtar-uretici" target="_blank">Kodları Github'dan İndir!</a><br>
-		<a href="https://github.com/emirtolis/rasgele-anahtar-uretici/blob/main/LICENSE" target="_blank">Lisans</a>
-	</center>
+	<button onclick="kopyala()">Anahtarı Kopyala</button>
+	<div class="github">
+		<hr>
+	    <a href="http://github.com/emirtolis/rasgele-anahtar-uretici" target="_blank">Kodları Github'dan İndir!</a><br>
+		<a href="http://github.com/emirtolis/rasgele-anahtar-uretici/blob/main/LICENSE" target="_blank">Lisans</a><hr>
+		<div class="footer">
+			<p>Emir Tolis tarafından <img src="http://cdn.r10.net/emojis/smile.png?site=" width="24px" alt="Gülen Yüz" title="emojis/smile.png"> ile yapıldı</p>
+		</div>
+	</div>
+	<script src="js/script.js"></script>
 </body>
 </html>
